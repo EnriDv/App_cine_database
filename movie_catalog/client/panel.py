26e13b4204971de17ae46618aacd17ede99bd9f6
MaBelenBecerra <maria.becerra.r@ucb.edu.bo>
2023-12-08 08:32:58 -0400

    Module create table
    se agrego el modulo de creacion de tablas

--- movie_catalog/client/panel.py
+++ movie_catalog/client/panel.py
@@ -13,11 +13,13 @@ class Panel:
         
         self.lbl_title= tk.Label(self.window, text="CINEMAX", font =("Bahnschrift SemiBold", 30),
                         bg="#a6192e", fg="white")
+        
         self.lbl_title.pack(side=tk.TOP, fill=tk.X)
+        self.var=tk.StringVar(self.window)
+        self.var.set('None')
 
         label=tk.Label(self.window, text="Select Genre:", font =("goudy old style", 18), bg="#fcfcfc").place(x=50, y =118)
-        self.select_genre()
-        boton = tk.Button(text='Obtain').place(x=350,y=123)
+        boton = tk.Button(text='Obtain', command=self.select_genre).place(x=350,y=123)
         self.window.mainloop()
         
         
@@ -27,43 +29,42 @@ class Panel:
         con.cursor.execute("""SELECT DISTINCT GENRE FROM MOVIE""")
         op = con.cursor.fetchall()
         con.exit()
-        self.var=tk.StringVar(self.window)
-        self.var.set('None')
-        opciones = op
+        opera = []
+        for n in op:
+            opera.append(n[0])
+        opciones = opera
         print(self.var.get())
         self.option = tk.OptionMenu(self.window, self.var, *opciones).place(x=220, y =120)
+        if self.var.get() == 'None':
+            pass
+        else:
+            self.create_table(str(self.var.get()))
         
 
-    def crear_tabla(self,gen):
+    def create_table(self,gen):
         con=Conect()
         con.cursor.execute("""SELECT TITLE, DURATION_, CLASIFICATION FROM MOVIE WHERE GENRE = :gen""", {'gen': gen})
         datos = con.cursor.fetchall()
 
-        # Crear ventana
-        ventana = tk.Tk()
-        ventana.title("Tabla")
+        window = tk.Tk()
+        window.title("Tabla")
 
-        # Crear tabla
-        tabla = ttk.Treeview(ventana)
-
-        # Definir columnas
-        tabla["Genre"] = ("Title", "Duration", "Clasification")
+        table = ttk.Treeview(window)
 
         # Configurar encabezados de columna
-        tabla.column("#0", width=0, stretch=tk.NO)  # Columna oculta
-        tabla.column("Title", anchor=tk.W, width=100)
-        tabla.column("Duration", anchor=tk.W, width=50)
-        tabla.column("Clasification", anchor=tk.W, width=100)
+        table.column("#0", width=0, stretch=tk.NO)  
+        table.column("Title", anchor=tk.W, width=100)
+        table.column("Duration", anchor=tk.W, width=80)
+        table.column("Clasification", anchor=tk.W, width=100)
 
-        # Configurar encabezado
-        tabla.heading("#0", text="", anchor=tk.W)
-        tabla.heading("Title", text="Title", anchor=tk.W)
-        tabla.heading("Duration", text="Duration", anchor=tk.W)
-        tabla.heading("Clasification", text="Clasification", anchor=tk.W)
+        table.heading("#0", text="", anchor=tk.W)
+        table.heading("Title", text="Title", anchor=tk.W)
+        table.heading("Duration", text="Duration", anchor=tk.W)
+        table.heading("Clasification", text="Clasification", anchor=tk.W)
 
         for dato in datos:
-            tabla.insert("", tk.END, values=dato)
-        tabla.pack()
-        ventana.mainloop()
+            table.insert("", tk.END, values=dato)
+        table.pack()
+       
         
         
